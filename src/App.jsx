96d370bc044ed7b032f39b1e7215ebf70b06fd78
JayPanchal-15092005 <trywebsiteapp56@gmail.com>
2025-03-20 16:04:31 +0530

    fix: remove unnecessary semicolon in Analytics import

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import CurrencyConverter from "./currencyConverter.jsx";
-import { Analytics } from "@vercel/analytics/react;"
+import { Analytics } from "@vercel/analytics/react";
 
 function App() {
   return (
