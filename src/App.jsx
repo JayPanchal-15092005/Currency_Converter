1d9db431d0f162d6b9737fd306f346e14bbdebf2
JayPanchal-15092005 <trywebsiteapp56@gmail.com>
2025-03-20 16:00:30 +0530

    add Vercel Analytics integration and update dependencies

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import React from "react";
 import CurrencyConverter from "./currencyConverter.jsx";
+import { Analytics } from "@vercel/analytics/react;"
 
 function App() {
   return (
     <div>
       <CurrencyConverter />
+      <Analytics />
     </div>
+    
   );
 }
 
